1d6b182ee73a00b869441be498da297b513d4e31
JungSuchang <jeongsc1001@gmail.com>
2016-08-29 17:45:34 +0900

    [Spring Scheduler] - Job 스케줄러 적용

---
+++ src/main/java/com/crontab/Scheduler.java
@@ -0,0 +1,41 @@
+package com.crontab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.samsung.iers.dao.task.TaskDAO;
+
+@Component
+public class Scheduler {
+	
+	
+	
+	@Autowired
+	private TaskDAO dao;
+	/**
+	 * 1. 오후 05:50:00에 호출이 되는 스케쥴러 
+	 */
+	@Scheduled(cron = "0 45 15 * * *")
+	public void cronTest1(){
+		
+		
+		
+		
+		Map<String, Object> commandMap = new HashMap<String, Object>();
+		
+		commandMap.putAll( dao.countTask(null));
+		System.out.println("오후 01:50:00에 호출이 됩니다. " + commandMap.get("TOTAL_COUNT"));
+	}
+	
+	/**
+	 * 2. 오후 05:51:00에 호출이 되는 스케쥴러 
+	 */
+	@Scheduled(cron = "0 * * * * *")
+	public void cronTest2(){
+		System.out.println("1분마다 돕니다. ");
+	}
+}
